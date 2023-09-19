fc8af5a2f99597d40754203e94ffc2bf2554b69a
Joop Stark <stark.joop@gmail.com>
2023-09-19 15:33:52 -0600

    refactor: top merchant remove dead space

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -31,15 +31,6 @@ class Merchant <ApplicationRecord
 
   def self.top_merchants
     find_by_sql(
-      # "SELECT merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_cost, MAX(invoices.updated_at) AS most_recent FROM merchants 
-	    # INNER JOIN items ON items.merchant_id = merchants.id 
-	    # INNER JOIN invoice_items ON invoice_items.item_id = items.id 
-	    # INNER JOIN invoices ON invoices.id = invoice_items.invoice_id
-	    # WHERE invoices.status = 0
-	    # GROUP BY merchants.id
-	    # ORDER BY total_cost DESC, most_recent DESC
-	    # LIMIT (5)"
-
       "SELECT merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_cost, MAX(invoices.updated_at) AS most_recent FROM merchants 
 	    INNER JOIN items ON items.merchant_id = merchants.id 
 	    INNER JOIN invoice_items ON invoice_items.item_id = items.id 
