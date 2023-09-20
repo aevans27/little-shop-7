2b83837d67560a7df88ce1f736ea48f59b9ede9f
Joop Stark <stark.joop@gmail.com>
2023-09-19 22:35:54 -0600

    refactor: advanced routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "welcome#index"
-
   namespace :admin do
     get "/", to: "dashboards#welcome"
-    get "/invoices", to: "invoices#index"
-    get "/invoices/:id", to: "invoices#show"
-    patch "/invoices/:id", to: "invoices#update"
-    
-    patch "merchants/:merchant_id", to: "merchants#update"
+    resources :invoices, only: [:index, :show, :update]
+    resources :merchants, except: :destroy
   end
-
-  get "/admin/merchants", to: "admin/merchants#index"
-  get "/admin/merchants/new", to: "admin/merchants#new"
-  post "admin/merchants", to: "admin/merchants#create"
-  get "/admin/merchants/:merchant_id", to: "admin/merchants#show" 
-  get "/admin/merchants/:merchant_id/edit", to: "admin/merchants#edit"
-  patch "/admin/merchants/:merchant_id", to: "admin/merchants#update"
   
   # namespace :merchant do
   #   get "/:merchant_id/"
@@ -28,14 +17,15 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/items", to: "merchant_items#index"
   get "/merchants/:merchant_id/items/new", to: "merchant_items#new"
   post "/merchants/:merchant_id/items", to: "merchant_items#create"
+
   get "/merchants/:merchant_id/items/:item_id", to: "merchant_items#show"
   patch "/merchants/:merchant_id/items/:item_id", to: "merchant_items#update"
   get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
-  get "/items/:item_id/edit", to: "items#edit"
-  patch "/items/:item_id", to: "items#update"
   get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show"
   patch "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#update"
-
+  
+  get "/items/:item_id/edit", to: "items#edit"
+  patch "/items/:item_id", to: "items#update"
 end
 # namespace: admin do 
 #   resources: merchants, only [index] 
